1d4de88d4a44fbb909a4347bab5fbfa8efdb2d38
Md Emdadullah <86929969+developeremdad@users.noreply.github.com>
2025-11-29 17:00:59 +0600

    added aggregation having challenge.

--- query/challenge.sql
+++ query/challenge.sql
@@ -126,7 +126,53 @@ the car has not been sold
 Group the table by brand.
 */
 
-
+/* 
 SELECT brand, count(brand), FLOOR(AVG(price)) as "AVG" FROM cars
 	WHERE sold IS FALSE
 	GROUP BY brand;
+ */
+
+/*
+	Select:
+		* the brand
+		* a count of the brand
+		* and an average of the price for each brand
+		* round the average down to the nearest number
+		* alias the average as 'AVG' in your output
+	From cars where
+		the car has not been sold
+	Group the table by brand.
+	
+	Show results where the count is > 1
+*/
+
+/* 
+SELECT brand, count(brand), FLOOR(AVG(price)) as "AVG" FROM cars
+	WHERE sold IS FALSE
+	GROUP BY brand
+	HAVING count(brand) > 1;
+ */
+
+ /*
+	Select:
+		* year
+		* a count of cars from that year, aliased as car_count
+		* the maximum price
+		* the minimum price
+	from the table cars
+		where the car has been sold
+	group by year
+		only show years where more than one car has been sold from that year
+	order the result by car_count
+*/
+
+
+SELECT year,
+	count(year) AS car_count,
+	MAX(price),
+	MIN(price)
+FROM cars
+WHERE sold IS TRUE
+GROUP BY year
+HAVING count(year) > 1
+ORDER BY car_count;
